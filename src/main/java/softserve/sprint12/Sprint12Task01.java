89c2caec55dd835a7307173d13e05d2d85266786
mykytam <morar.nikita@gmail.com>
2020-05-07 21:18:19 +0300

    Sprint 12. Task 1. Init commit

---
+++ src/main/java/softserve/sprint12/Sprint12Task01.java
@@ -0,0 +1,23 @@
+package softserve.sprint12;
+
+//Implement class ParallelCalculator that will be able to execute an operation in parallel thread.
+//Use the implementation of Runnable for this.
+//Constructor of ParallelCalculator should take 3 parameters:
+//BinaryOperator<Integer> to define an operation that will be executed,
+//int operand1 and int operand2.
+//ParallelCalculator class should have not private int result field where the result of the operation will be written when it's executed.
+
+import java.util.function.BinaryOperator;
+
+class ParallelCalculator implements Runnable {
+    int result;
+
+    public ParallelCalculator(BinaryOperator<Integer> operator, int operand1, int operand2) {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
